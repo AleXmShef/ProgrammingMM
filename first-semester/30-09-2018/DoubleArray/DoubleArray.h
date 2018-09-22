7d3b17d69f74a172db046d16e63b1c0054e57bdc
yuna <bolshakovayd@gmail.com>
2018-09-22 10:49:16 +0300

    Add DoubleArray

---
+++ first-semester/30-09-2018/DoubleArray/DoubleArray.h
@@ -0,0 +1,31 @@
+//
+// Created by yuna on 21.09.18.
+//
+
+#ifndef DOUBLEARRAY_DOUBLEARRAY_H
+#define DOUBLEARRAY_DOUBLEARRAY_H
+
+struct DoubleArray {
+    explicit DoubleArray(unsigned int size);
+
+    DoubleArray(unsigned int size, double defaultValue);
+
+    ~DoubleArray();
+
+    double &operator[](unsigned int index);
+
+    void print();
+
+    unsigned int size();
+
+    void sort();
+
+private:
+    unsigned int sizeOfArray;
+    double *data;
+
+};
+
+void quickSort(double *arr[], int left, int right);
+
+#endif //DOUBLEARRAY_DOUBLEARRAY_H
