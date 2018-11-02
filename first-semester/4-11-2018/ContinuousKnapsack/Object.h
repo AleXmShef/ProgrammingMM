db510ba42e2bfbf0964666ff5aae835be270db70
yuna <bolshakovayd@gmail.com>
2018-11-02 20:32:16 +0300

    Add Continuous knapsack problem

---
+++ first-semester/4-11-2018/ContinuousKnapsack/Object.h
@@ -0,0 +1,16 @@
+//
+// Created by yuna on 02.11.18.
+//
+
+#ifndef CONTINUOUSKNAPSACK_OBJECT_H
+#define CONTINUOUSKNAPSACK_OBJECT_H
+
+#include <string>
+
+struct Object {
+    std::string name;
+    unsigned int weight;
+    unsigned int price;
+};
+
+#endif //CONTINUOUSKNAPSACK_OBJECT_H
