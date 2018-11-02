5a88d553e429b501f181c1e0e53aac9eb03d237f
yuna <bolshakovayd@gmail.com>
2018-11-02 18:22:19 +0300

    Add backpack

---
+++ first-semester/11-11-2018/backpack/Object.h
@@ -0,0 +1,17 @@
+//
+// Created by yuna on 02.11.18.
+//
+
+#ifndef BACKPACK_OBJECT_H
+#define BACKPACK_OBJECT_H
+
+#include <string>
+
+struct Object {
+    std::string name;
+    unsigned int weight;
+    unsigned int price;
+};
+
+
+#endif //BACKPACK_OBJECT_H
---
+++ first-semester/28-10-2018/k-permutation/cmake-build-debug/CMakeFiles/k_permutation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/k_permutation.dir/main.cpp.o"
+  "k_permutation.pdb"
+  "k_permutation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/k_permutation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
