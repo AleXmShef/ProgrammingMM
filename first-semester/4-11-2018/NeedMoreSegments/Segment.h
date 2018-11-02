e34be82e141eb78ebaf106db96bc0192700b5a7f
yuna <bolshakovayd@gmail.com>
2018-11-02 21:12:22 +0300

    Add segments problem

---
+++ first-semester/4-11-2018/NeedMoreSegments/Segment.h
@@ -0,0 +1,13 @@
+//
+// Created by yuna on 02.11.18.
+//
+
+#ifndef NEEDMORESEGMENTS_SEGMENT_H
+#define NEEDMORESEGMENTS_SEGMENT_H
+
+struct Segment {
+    int begining;
+    int ending;
+};
+
+#endif //NEEDMORESEGMENTS_SEGMENT_H
---
+++ first-semester/4-11-2018/NeedMoreSegments/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "Segment.h"
+
+using namespace std;
+
+int main() {
+    std::cout << "Enter a number segments: ";
+    int n;
+    cin >> n;
+    vector<Segment> segments;
+    std::cout << "Enter beginings and endings of segments lying in the interval [0,100]: ";
+    for (auto i = 0; i < n; ++i) {
+        Segment temp;
+        cin >> temp.begining >> temp.ending;
+        segments.push_back(temp);
+    }
+    sort(segments.begin(), segments.end(),
+         [](Segment a, Segment b) {
+             return a.ending < b.ending;
+         }
+    );
+
+    for (auto i = 0; i < segments.size(); ++i) {
+        cout << segments[i].begining << " " << segments[i].ending << endl;
+    }
+
+
+    int ans = 1;
+
+
+    for (auto i = 1; i < segments.size(); ++i) {
+        if (segments[i - 1].ending > segments[i].begining) {
+            segments.erase(segments.begin() + i);
+            --i;
+            continue;
+        }
+        if (segments[i].ending <= 100) {
+            ans++;
+        } else {
+            break;
+        }
+    }
+    cout << "Maximum number of non-intersecting segments is " << ans;
+    return 0;
+}
